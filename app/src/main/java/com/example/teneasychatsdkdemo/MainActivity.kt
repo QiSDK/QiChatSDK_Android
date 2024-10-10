344e6ed515de29cfbb6d3ab7723336872acbcc4f
xiao fu <7938813+xiaofu2024@users.noreply.github.com>
2024-10-10 15:21:30 +0800

    代码混淆

--- app/src/main/java/com/example/teneasychatsdkdemo/MainActivity.kt
+++ app/src/main/java/com/example/teneasychatsdkdemo/MainActivity.kt
@@ -114,7 +114,7 @@ class MainActivity : AppCompatActivity(), TeneasySDKDelegate {
                 Log.i("LineLib", "使用线路："+ line)
                 appendText("Wss: " + line + "\n")
                 domain = line
-                    initChatSDK(line)
+                initChatSDK(line)
             }
             override fun lineError(error: Result) {
                 if (error.code == 1008){
@@ -134,7 +134,7 @@ class MainActivity : AppCompatActivity(), TeneasySDKDelegate {
         var wssUrl = "wss://" + baseUrl + "/v1/gateway/h5?"
         //token: COYBEAIYzNdEIPIBKJDZrOP3MQ.maPNGL2-vih71Eg4ghU4aTMSY6Sl0Zt8GTH6colScbTZQiTM5hak9do9qyxvhxSes-HuKbsNMLlBE72Z3J-4Bg
         //666668，364154
-        chatLib = ChatLib("COYBEAUYASDyASiG2piD9zE.te46qua5ha2r-Caz03Vx2JXH5OLSRRV2GqdYcn9UslwibsxBSP98GhUKSGEI0Z84FRMkp16ZK8eS-y72QVE2AQ", "", wssUrl, 666665, "9zgd9YUc", 0, "", 3)
+        chatLib = ChatLib("COYBEAUYASDyASiG2piD9zE.te46qua5ha2r-Caz03Vx2JXH5OLSRRV2GqdYcn9UslwibsxBSP98GhUKSGEI0Z84FRMkp16ZK8eS-y72QVE2AQ", "", wssUrl, 666665, "9zgd9YUc", 0, "", 5)
         chatLib.listener = this
         chatLib.makeConnect()
 
